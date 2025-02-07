160e584fa179cd433a547d61d97ed4a99382f79e
ryosuke <rogerrafarn0324@gmail.com>
2025-02-07 11:29:06 +0900

    .

--- generate_password.py
+++ generate_password.py
@@ -7,7 +7,7 @@ def generate_password(length, use_uppercase, use_symbols):
     if use_uppercase:
         characters += string.ascii_uppercase
     if use_symbols:
-        characters += "*+@¥$#&=<>"
+        characters += "-!#$%&()*,./:;?@[]^_`{|}~+<=>"
 
     return ''.join(secrets.choice(characters) for _ in range(length))
 
